9e1913ccee55bae5ead59d85b4ffac3e345bc93d
Realyn Vasquez <nealry@gmail.com>
2026-06-29 21:14:07 +0100

    deplot to gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Scramble from './pages/Scramble'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/scramble" element={<Scramble />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
